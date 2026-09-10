7e662d4d2b0269c31a5fd81a557628cc134b393b
primary-null <primary-null@users.noreply.github.com>
2026-09-09 21:36:20 -0700

    0.7.2 Fold inner two-pane

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.pxmx.app"
         minSdk = 27
         targetSdk = 36
-        versionCode = 33
-        versionName = "0.6.16"
+        versionCode = 34
+        versionName = "0.7.2"
         testInstrumentationRunner = "com.pxmx.app.CustomTestRunner"
     }
 
